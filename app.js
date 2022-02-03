2a7876a3ecf14e635175ec5952b53dd52376eba8
ankur.goyal <ankur.goyal@op.fi>
2022-02-03 12:33:21 +0200

    changed one line again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Something was here!'));
+app.get('/', (req, res) => res.send('Something was here and now its not!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
